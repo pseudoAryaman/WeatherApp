3ca026fef23ffc63ad12c86a052ec865610f246d
Aryaman <yes.waryaman@gmail.com>
2026-04-27 21:32:26 +0530

    Updated Files

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -2,13 +2,5 @@ import React from "react";
 
 export default function Credit() {
   return (
-    <div class="credit">
-    <p>
-      Coded by
-      <a href="https://www.shecodes.io/graduates/73033-chaimae-lamirine"
-        > Chaimae Lamirine</a
-      >, <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
-    </p>
-  </div>
-  );
-}
+    <div class="credit"></div>
+  )};
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -10,7 +10,7 @@ function Forecast({ weather }) {
   useEffect(() => {
     const fetchForecastData = async () => {
       const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-      const url = `https://api.shecodes.io/weather/v1/forecast?query=${data.city}&key=${apiKey}&units=metric`;
+      const url = `https://api.shecodes.iogit /weather/v1/forecast?query=${data.city}&key=${apiKey}&units=metric`;
 
       try {
         const response = await axios.get(url);
